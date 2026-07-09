70d8435bdc35e5ee3c123b6dd5891b96aed40253
ssr <jpr_surendra@yahoo.co.in>
2026-07-09 11:57:51 +0530

    new project created

---
+++ src/main/java/com/idp/CentralIdpApplication.java
@@ -0,0 +1,13 @@
+package com.idp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralIdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralIdpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idp/CentralIdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralIdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
